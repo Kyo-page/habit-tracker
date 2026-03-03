ee35fa81faa74dbc34a9ff8d999e139996cc6048
Kyoka <yuzz025@gmail.com>
2026-03-03 13:38:20 +0900

    feat: カレンダーのグリッド表示とレイアウトの最適化

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import type { DailyRecord, Level } from "./types/types";
+import type { DailyRecord } from "./types/types";
 import { Square } from "./components/Square";
 
 // ダミーデータ
@@ -14,24 +14,46 @@ const dummyData: DailyRecord = {
     "2026-02-10": 3,
     "2026-02-11": 4,
     "2026-02-12": 0,
+    "2026-02-14": 4,
     "2026-02-13": 1,
+    "2026-02-15": 2,
+    "2026-02-16": 3,
+    "2026-02-17": 4,
+    "2026-02-18": 0,
+    "2026-02-19": 1,
+    "2026-02-20": 2,
+    "2026-02-21": 3,
+    "2026-02-22": 4,
+    "2026-02-23": 0,
+    "2026-02-24": 1,
+    "2026-02-25": 2,
+    "2026-02-26": 3,
+    "2026-02-27": 4,
+    "2026-02-28": 0,
+    "2026-03-01": 1,
+    "2026-03-02": 2,
+    "2026-03-03": 3,
 };
 
 function App() {
     const [records, setRecords] = useState<DailyRecord>(dummyData);
 
     // オブジェクトを「キー（日付）」と「値（Level）」のペアの配列に変換
-    const recordEntries = Object.entries(records);
+    const recordEntries = Object.entries(records).sort((a, b) => a[0].localeCompare(b[0]));
 
     return (
-        <div className="container p-6 space-y-4">
+        <div className="p-6 space-y-4">
             <h1 className="text-3xl font-bold">Habit Tracker</h1>
-            <div className="flex flex-wrap gap-1 w-full max-w-4xl">
-                {/* map の引数で分割代入を使って受け取る */}
-                {recordEntries.map(([date, level]) => (
-                    // タイルを並べる
-                    <Square key={date} level={level} />
-                ))}
+            <div className="card">
+                <div className="card-body space-y-4 items-start">
+                    <div className="inline-flex flex-wrap flex-col gap-1 max-h-34">
+                        {/* map の引数で分割代入を使って受け取る */}
+                        {recordEntries.map(([date, level]) => (
+                            // タイルを並べる
+                            <Square date={date} key={date} level={level} />
+                        ))}
+                    </div>
+                </div>
             </div>
         </div>
     );
